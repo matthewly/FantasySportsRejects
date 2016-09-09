f4eb7ac22effa5494ed6a9c73e9e6596eb4b6712
Matthew Ly <matthew.ly@mly-01.workdayinternal.com>
2016-09-08 17:30:22 -0700

    added socket.io dependency

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-
+var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname));
@@ -12,16 +12,24 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
 
 app.post('/', function (req, res) {
   
+  //io.emit('text', req.body.Body);
+  console.log(req.body.Body);
+  var text_content = req.body.Body;
 
-  res.json({text: "test"});
+
+  res.json({text: req.body.Body});
 });
 
 
-
+io.on('connection', function(socket){
+  console.log('connected');
+});
 
 http.listen(4000, function () {
   console.log('listening on port 4000!');
